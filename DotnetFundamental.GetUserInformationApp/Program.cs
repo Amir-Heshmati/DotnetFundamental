757b22ed66e2dfad70dbe4e769cb833cac9c464e
Amir Heshmati <am16he@gmail.com>
2026-07-21 14:15:55 +0330

    Create GetUserInformationApp Project

---
+++ DotnetFundamental.GetUserInformationApp/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DotnetFundamental.GetUserInformationApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, User!");
+            
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+            
+            Console.WriteLine("What is your phone number?");
+            string phone = Console.ReadLine();
+            
+            Console.WriteLine("What is your national code?");
+            string nationalCode = Console.ReadLine();
+            
+            Console.WriteLine("What is your card number?");
+            string cardNumber = Console.ReadLine();
+        }
+    }
+}
